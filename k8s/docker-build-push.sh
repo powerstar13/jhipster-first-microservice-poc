47e02f0965cc41f5fb98159280631e43594da9fb
홍준성 <jshong@sdt.inc>
2025-01-22 10:52:47 +0900

    Build docker

--- k8s/docker-build-push.sh
+++ k8s/docker-build-push.sh
@@ -1,1 +1,1 @@
-docker buildx build -t first-microservice:latest --platform linux/amd64,linux/arm64 ..
+docker buildx build -t jhipster-poc/first-microservice:latest --platform linux/amd64,linux/arm64 ..
